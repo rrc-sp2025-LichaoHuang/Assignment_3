3fe7f0b7354dd3cb2fa2a3c7ee58902bcf33f9e1
Lichao Huang <lhuang38@rrc.ca>
2025-06-15 19:02:57 -0500

    Process Transactions Testing

--- automatic_teller_machine.py
+++ automatic_teller_machine.py
@@ -3,10 +3,12 @@ Lichao_Huang
 06/15/2025
 """
 import random
-Deposit = ['D','d']
-Withdrow = ['W','w']
-Quit = ['Q','q']
+Command = ('D','d','W','w','Q','q')
+Deposit = ('D','d')
+Withdrow = ('W','w')
+Quit = ('Q','q')
 balacne = int(random.randint(-1000,10000))
+
 
 a = "****************************************"
 b = "PIXELL RIVER FINANCIAL"
@@ -15,6 +17,9 @@ d = " Your current balance is:$"
 e = "Deposit: D"
 f = "Withdraw: W"
 g = "Quit: Q"
+h = "Enter your selection:"
+i = "INVALID SELECTION"
+j = "INSUFFICIENT FUNDS"
 
 print('{:^40}'.format(a))
 print('{:^40}'.format(b))
@@ -26,3 +31,34 @@ print('{:^40}'.format(e))
 print('{:^40}'.format(f))
 print('{:^40}'.format(g))
 print('{:^40}'.format(a))
+
+selection = input(h)
+while selection in Command:
+    if selection in Deposit:
+        amount = float(input('Enter the transaction amount:'))
+        balacne = balacne + amount
+        print()
+        print('{:^40}'.format(a))
+        print(f'{d}{balacne:,.2f}')
+        print('{:^40}'.format(a))
+    if selection in Withdrow:
+        if balacne > 0 :
+            amount = float(input('Enter the transaction amount:'))
+            if amount < balacne:
+                balacne = balacne - amount
+                print()
+                print('{:^40}'.format(a))
+                print(f'{d}{balacne:,.2f}')
+                print('{:^40}'.format(a))
+            else:
+                print()
+                print('{:^40}'.format(a))
+                print('{:^40}'.format(j))
+                print('{:^40}'.format(a))
+        else:
+            print()
+            print('{:^40}'.format(a))
+            print('{:^40}'.format(j))
+            print('{:^40}'.format(a))
+    if selection in Quit:
+        break
