5846bdfe93d3169a25570ee5050ec9aebff9d677
Lichao Huang <lhuang38@rrc.ca>
2025-06-16 00:08:04 -0500

    Process Accounts

--- interest_update.py
+++ interest_update.py
@@ -9,5 +9,25 @@ with open("account_balances.txt", "r") as input_file:
     for line in input_file:
         key, value = line.strip().split('|')
         data[key] = float(value)
-print(data)
+pprint(data)
+
+for key,value in data.items():
+    if value < 0:
+        value = (value*1.1/12) + value
+        print(value)
+        data[key] = value
+    if value == 0:
+        data[key] = value
+    if 0 < value < 1000:
+        value = (value*1.01/12) + value
+        data[key] = value
+    if 1000 <= value < 5000:
+        value = (value*1.025/12) + value
+        data[key] = value
+    if 5000 <= value:
+        value = (value*1.05/12) + value
+        data[key] = value
+
+
+data["Account"] = ["Balance"]
 pprint(data)
